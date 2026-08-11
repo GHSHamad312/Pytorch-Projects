994204cfebefb47213a5c093f13e92a8a1affb8e
GHSHamad312 <ghshamad312@gmail.com>
2026-08-11 22:24:14 +0500

    feat: implement CustomDataset and random data splitting for industrial maintenance dataset

--- test_train_split/random_splitter.py
+++ test_train_split/random_splitter.py
@@ -5,10 +5,19 @@ import pandas as pd
 class CustomDataset(Dataset):
     def __init__(self, file):
         df=pd.read_csv(file)
-        self.x=torch.tensor(df[["equipment_id","operating_hours","rotation_speed_rpm","torque_nm","power_kw","ambient_temp_c","bearing_temp_c","vibration_amplitude_mm","oil_viscosity_cst","coolant_pressure_bar","acoustic_noise_db","voltage_fluctuation_v","prior_maintenance_count","error_log_count_24h","remaining_useful_life_days","equipment_failure_risk"]], dtype=torch.float32)
-        self.y=torch.tensor(df["failure_mode_type"])
-    def __length__(self):
+        self.x=torch.tensor(df[["operating_hours","rotation_speed_rpm","torque_nm","power_kw","ambient_temp_c"]].values, dtype=torch.float32)
+        self.y=torch.tensor(df["failure_mode_type"].values, dtype=torch.long)
+    def __len__(self):
         return len(self.x)
 
     def __getitem__(self,index):
         return self.x[index],self.y[index]
+
+dataset=CustomDataset("./test_train_split/industrial_predictive_maintenance_data.csv")
+test_split, train_split=random_split(dataset,[0.2,0.8])
+print(f'test split length:{len(test_split)}')
+print(f'trian split length:{len(train_split)}')
+
+train_loader=DataLoader(train_split, batch_size=32, shuffle=True)
+test_loader=DataLoader(test_split, batch_size=32, shuffle=False)
+
