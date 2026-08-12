abe98f723f616d48fefe693b21eb0199c2f8b63c
GHSHamad312 <ghshamad312@gmail.com>
2026-08-12 23:06:51 +0500

    feat: implement data loading and missing value imputation for industrial maintenance dataset

--- pandas_for_machineLearning/filtering.py
+++ pandas_for_machineLearning/filtering.py
@@ -1,3 +1,14 @@
 import pandas as pd
 
 dataframe=pd.read_csv("./pandas_for_machineLearning/industrial_maintenance_with_missing_values.csv")
+# print(dataframe.count())
+# print(dataframe.isnull().sum())
+# dataframe=dataframe.dropna()
+# print(dataframe.isnull().sum())
+# print(dataframe.count())
+
+# filling nan's
+mean=dataframe['operating_hours'].mean()
+dataframe['operating_hours']=dataframe['operating_hours'].fillna(mean)
+print(dataframe.isnull().sum())
+print(dataframe.duplicated().sum())
